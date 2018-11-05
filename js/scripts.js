920c1ea0fec3618ecac576219d521816c118d064
pavelp21 <pavelvp2182@yandex.ru>
2018-11-05 17:54:53 +0500

    Main optimizations

--- js/scripts.js
+++ js/scripts.js
@@ -104,6 +104,24 @@ $(document).ready(function () {
 		var a = $(this).parent().attr("data-youtube");
 		$(this).parent().html('<iframe src="https://www.youtube.com/embed/' + a + '?showinfo=0&rel=0&autoplay=1" allowfullscreen></iframe>')
 	});
-
+	/*Страые браузеры*/
+	var yaBrowserUpdater = new ya.browserUpdater.init({
+		"lang": "ru",
+		"browsers": {
+			"yabrowser": "15.10",
+			"chrome": "52",
+			"ie": "10",
+			"opera": "40",
+			"safari": "8",
+			"fx": "47",
+			"iron": "35",
+			"flock": "Infinity",
+			"palemoon": "25",
+			"camino": "Infinity",
+			"maxthon": "4.5",
+			"seamonkey": "2.3",
+			"theme": "blue"
+		}
+	});
 	/*Конец документа*/
 });
